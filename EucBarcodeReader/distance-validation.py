172e6afe1be04e8fd957f45a55c0b5a8f00ad6f8
James Kondilios <u6046902@anu.edu.au>
2018-03-13 23:03:29 +1100

    Photos appear to not be GPS tagged. They were potentially imported incorrectly. Try through iPhoto. (were done over facebook)

--- EucBarcodeReader/distance-validation.py
+++ EucBarcodeReader/distance-validation.py
@@ -1,8 +1,9 @@
 import zlib
 import glob
-import numpy as np
 import zbarlight
 from PIL import Image
+import piexif as pe
+import exifread
 
 
 files = glob.glob("/Users/jameskonda/Desktop/Genomics/EucBarcodeReader/TestPhotos/*.jpg")
@@ -12,3 +13,20 @@ for f in files:
     codes = zbarlight.scan_codes('qrcode', im)
     code = codes[0].decode('utf8') if codes else 'unknown'
     print(f + ": " + 'QR codes: %s' % code)
+    tags = exifread.process_file(open(str(f), 'rb')) # none of this works
+    geo = {i:tags[i] for i in tags.keys() if i.startswith('GPS')} # this does not works
+
+    print(geo) # returns only empty brackets - look into
+
+    
+
+
+#im = Image.open(f)
+
+#exif_dict = pe.load("/Users/jameskonda/Desktop/Genomics/EucBarcodeReader/TestPhotos/1m.jpg")
+#exif_dict = pe.load(im)
+#print(exif_dict)
+
+
+#source activate barcode-reader
+#/Users/jameskonda/Desktop/Genomics/EucBarcodeReader
