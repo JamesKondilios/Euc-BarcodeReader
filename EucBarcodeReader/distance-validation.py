f9bc8ed02c3bba24e131c0ca7f29e87948fff467
James Kondilios <u6046902@anu.edu.au>
2018-03-15 11:17:51 +1100

    Convert vomit GPS data into latitude, longditude and altitude with corrections for hemisphere

--- EucBarcodeReader/distance-validation.py
+++ EucBarcodeReader/distance-validation.py
@@ -29,8 +29,23 @@ for f in files:
     decoded = dict((TAGS.get(key, key), value) for key, value in info.items())
     print(decoded.get('GPSInfo'))
 
+    lat = [float(x) / float(y) for x, y in decoded['GPSInfo'][2]]
+    lat = lat[0] + lat[1] / 60
+    lon = [float(x) / float(y) for x, y in decoded['GPSInfo'][4]]
+    lon = lon[0] + lon[1] / 60
+    alt = float(decoded['GPSInfo'][6][0]) / float(decoded['GPSInfo'][6][1])
+
+    if decoded['GPSInfo'][1] == "S":
+        lat *= -1
+    if decoded['GPSInfo'][3] == "W":
+        lon *= -1
+
+    print(lat,lon,alt)
+
 
 
 # photos had to be exported from iphoto. File > export > Export Unmodified Original
 #source activate barcode-reader
 #/Users/jameskonda/Desktop/Genomics/EucBarcodeReader
+
+# Iphone GPS data is a little bit off :/
