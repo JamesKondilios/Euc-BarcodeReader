6ad5a6f1fcbfdf788b5e97d506db01e8074e4f43
James Kondilios <u6046902@anu.edu.au>
2018-03-15 11:04:12 +1100

    able to successfully pull out GPS exif data]

--- EucBarcodeReader/distance-validation.py
+++ EucBarcodeReader/distance-validation.py
@@ -2,31 +2,35 @@ import zlib
 import glob
 import zbarlight
 from PIL import Image
-import piexif as pe
 import exifread
+from PIL.ExifTags import TAGS, GPSTAGS
 
+# photos had to be exported from iphoto. File > export > Export Unmodified Original
+#source activate barcode-reader
+#/Users/jameskonda/Desktop/Genomics/EucBarcodeReader
 
-files = glob.glob("/Users/jameskonda/Desktop/Genomics/EucBarcodeReader/TestPhotos/*.jpg")
+def path_leaf(path):
+    """ guaranteed filename from path; works on Win / OSX / *nix """
+    head, tail = ntpath.split(path)
+    return tail
+print("HI")
+
+files = glob.glob("/Users/jameskonda/Desktop/Genomics/EucBarcodeReader/TestPhotos/*.JPG")
 
 for f in files:
     im = Image.open(f)
     codes = zbarlight.scan_codes('qrcode', im)
     code = codes[0].decode('utf8') if codes else 'unknown'
     print(f + ": " + 'QR codes: %s' % code)
-    tags = exifread.process_file(open(str(f), 'rb')) # none of this works
-    geo = {i:tags[i] for i in tags.keys() if i.startswith('GPS')} # this does not works
 
-    print(geo) # returns only empty brackets - look into
+    img = Image.open(f)
+    info = img._getexif()
 
-    
-
-
-#im = Image.open(f)
-
-#exif_dict = pe.load("/Users/jameskonda/Desktop/Genomics/EucBarcodeReader/TestPhotos/1m.jpg")
-#exif_dict = pe.load(im)
-#print(exif_dict)
+    decoded = dict((TAGS.get(key, key), value) for key, value in info.items())
+    print(decoded.get('GPSInfo'))
 
 
+
+# photos had to be exported from iphoto. File > export > Export Unmodified Original
 #source activate barcode-reader
 #/Users/jameskonda/Desktop/Genomics/EucBarcodeReader
